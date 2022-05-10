c28580fa6e7eb18398831f90781ebd59b047543a
Tity.S <tity.septiani@gmail.com>
2022-05-10 23:49:08 +0700

    Sets the data source for the tableview

--- EDQuizApp/ResultViewController.swift
+++ EDQuizApp/ResultViewController.swift
@@ -8,18 +8,28 @@
 import Foundation
 import UIKit
 
-class ResultViewController:UIViewController {
+class ResultViewController:UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var headerLabel: UILabel!
     
     private var summary = ""
+    private var answers = [String]()
     convenience init(summary:String, answers:[String]) {
         self.init()
         self.summary = summary
+        self.answers = answers
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         headerLabel.text = summary
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
