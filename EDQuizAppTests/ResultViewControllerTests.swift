b90cbcb69e970830955df2f3e001d4ce0de985a4
Tity.S <tity.septiani@gmail.com>
2022-05-10 23:47:01 +0700

    Add failing test test_viewDidLoad_withOneAnswer_rendersAnswers() for rendering one data on tableview

--- EDQuizAppTests/ResultViewControllerTests.swift
+++ EDQuizAppTests/ResultViewControllerTests.swift
@@ -16,9 +16,17 @@ class ResultViewControllerTests:XCTestCase {
         XCTAssertEqual(sut.headerLabel.text, "a summary")
     }
     
+    // On testing the tableview it always starts with nothing on the table
+    // and then it will gradually add one or two data inside the table and check if the data renders correctly
     func test_viewDidLoad_withoutAnswers_doesNotRenderAnswers() {
         let sut = ResultViewController(summary: "a summary", answers:[])
         _ = sut.view
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_viewDidLoad_withOneAnswer_rendersAnswers() {
+        let sut = ResultViewController(summary: "a summary", answers:["A1"])
+        _ = sut.view
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
     }
 }
