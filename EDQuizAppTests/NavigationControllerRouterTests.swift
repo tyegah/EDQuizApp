96b98ced05e92bfa69d475b104ff5e3708248327
Tity.S <tity.septiani@gmail.com>
2022-05-29 23:17:59 +0700

    - Create the test test_routeToQuestion_presentsQuestionControllerWithCorrectAnswerCallback() to test the correct callback for answer

--- EDQuizAppTests/NavigationControllerRouterTests.swift
+++ EDQuizAppTests/NavigationControllerRouterTests.swift
@@ -85,7 +85,32 @@ class NavigationControllerRouterTests:XCTestCase {
         XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
     }
     
+    // Previously we only tested the question and viewcontroller
+    // Here we need to make sure the answercallback is correctly fired
+    func test_routeToQuestion_presentsQuestionControllerWithCorrectAnswerCallback() {
+        // 3. Setup the required classes (like DI classes) and the SUT
+        // first, we need the navigation controller
+        let navigationController = UINavigationController()
+        // we also need to create the stub of the factory because it is now a requirement to create the SUT
+        let viewControllerFactoryStub = ViewControllerFactoryStub()
+        // we stub the viewcontrollers into the factory
+        let viewController = UIViewController()
+        viewControllerFactoryStub.stub(question: "Q1", with: viewController)
+        let secondViewController = UIViewController()
+        viewControllerFactoryStub.stub(question: "Q2", with: secondViewController)
+        // then, we need the SUT, which is a class that's not created yet but will be created at this point
+        let sut = NavigationControllerRouter(navigationController, factory: viewControllerFactoryStub)
+        // 2. Call the functionality that we're testing
+        var callbackFired = false
+        sut.routeTo(question: "Q1", answerCallback: { _ in  callbackFired = true})
+        viewControllerFactoryStub.answerCallbacks["Q1"]!("Answer")
+        // 1. determine the expected result
+        XCTAssertTrue(callbackFired)
+    }
+    
     class ViewControllerFactoryStub: ViewControllerFactory {
+        // We hold the answer callbacks here
+        var answerCallbacks = [String: (String) -> Void]()
         // We hold the value of the stubbed viewcontrollers here
         private var stubbedQuestions = [String:UIViewController]()
         
@@ -95,6 +120,7 @@ class NavigationControllerRouterTests:XCTestCase {
         }
         
         func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController {
+            self.answerCallbacks[question] = answerCallback
             return stubbedQuestions[question]!
         }
     }
