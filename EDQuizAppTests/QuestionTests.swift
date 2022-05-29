9b124f40439e6dd557205a2fff2d650d9d0a057d
Tity.S <tity.septiani@gmail.com>
2022-05-30 00:00:34 +0700

    - Create Question as enum - Create QuestionTests test_hashValue_singleAnswer_returnsTypeHash() for checking hashvalue on single answer

---
+++ EDQuizAppTests/QuestionTests.swift
@@ -0,0 +1,28 @@
+//
+//  QuestionTests.swift
+//  EDQuizAppTests
+//
+//  Created by Ty Septiani on 29/05/22.
+//
+
+import Foundation
+import XCTest
+@testable import EDQuizApp
+
+class QuestionTests: XCTestCase {
+    func test_hashValue_singleAnswer_returnsTypeHash() {
+        let type = "a string"
+        let sut = Question.singleAnswer(type)
+        XCTAssertEqual(sut.hashValue, type.hashValue)
+    }
+    
+//    func test_hashValue_multipleAnswer_returnsTypeHash() {
+//        let type = "a string"
+//        let sut = Question.multipleAnswer(type)
+//        XCTAssertEqual(sut.hashValue, type.hashValue)
+//    }
+//    
+//    func test_equal_singleAnswer_isEqual() {
+//        XCTAssertEqual(Question.singleAnswer("a string"), Question.singleAnswer("a string"))
+//    }
+}
