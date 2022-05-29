9b915f4d04bdd1b9ff8abb686afb08a3c4fb0aac
Tity.S <tity.septiani@gmail.com>
2022-05-30 00:06:16 +0700

    - Create test test_notEqual_singleAnswer_isNotEqual() and pass the test by adding implementation inside the == function

--- EDQuizApp/NavigationControllerRouter.swift
+++ EDQuizApp/NavigationControllerRouter.swift
@@ -29,7 +29,12 @@ enum Question<T:Hashable>:Hashable {
     
     // we need to implement this because of the hashable protocol
     static func ==(lhs:Question<T>, rhs:Question<T>) -> Bool {
-        return true
+        switch (lhs, rhs) {
+        case (.singleAnswer(let a), .singleAnswer(let b)):
+            return a == b
+        default:
+            return false
+        }
     }
 }
 
--- EDQuizAppTests/QuestionTests.swift
+++ EDQuizAppTests/QuestionTests.swift
@@ -22,9 +22,17 @@ class QuestionTests: XCTestCase {
         let sut = Question.multipleAnswer(type)
         XCTAssertEqual(sut.hashValue, type.hashValue)
     }
-
+    
     // Equatable tests
     func test_equal_singleAnswer_isEqual() {
         XCTAssertEqual(Question.singleAnswer("a string"), Question.singleAnswer("a string"))
     }
+    
+    func test_notEqual_singleAnswer_isNotEqual() {
+        XCTAssertNotEqual(Question.singleAnswer("a string"), Question.singleAnswer("another string"))
+    }
+    
+//    func test_equal_multipleAnswer_isEqual() {
+//        XCTAssertEqual(Question.multipleAnswer("a string"), Question.multipleAnswer("a string"))
+//    }
 }
